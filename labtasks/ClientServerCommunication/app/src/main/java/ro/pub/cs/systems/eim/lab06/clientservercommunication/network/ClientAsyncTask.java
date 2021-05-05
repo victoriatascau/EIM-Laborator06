f5a5cc12fd3aadd17a1400cfc0228d4e6682ec4a
victoriatascau <vickytascau@gmail.com>
2021-05-05 23:49:39 +0300

    Implement task 6

--- labtasks/ClientServerCommunication/app/src/main/java/ro/pub/cs/systems/eim/lab06/clientservercommunication/network/ClientAsyncTask.java
+++ labtasks/ClientServerCommunication/app/src/main/java/ro/pub/cs/systems/eim/lab06/clientservercommunication/network/ClientAsyncTask.java
@@ -1,10 +1,16 @@
 package ro.pub.cs.systems.eim.lab06.clientservercommunication.network;
 
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
 import ro.pub.cs.systems.eim.lab06.clientservercommunication.general.Constants;
+import ro.pub.cs.systems.eim.lab06.clientservercommunication.general.Utilities;
 
 public class ClientAsyncTask extends AsyncTask<String, String, Void> {
 
@@ -16,20 +22,44 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
 
     @Override
     protected Void doInBackground(String... params) {
+        Socket socket = null;
         try {
 
             // TODO exercise 6b
             // - get the connection parameters (serverAddress and serverPort from parameters - on positions 0 and 1)
+            String address = params[0];
+            int port = Integer.parseInt(params[1]);
             // - open a socket to the server
+            socket = new Socket(address, port);
+            if (socket == null) {
+                return null;
+            }
             // - get the BufferedReader in order to read from the socket (use Utilities.getReader())
+            BufferedReader bufferedReader = Utilities.getReader(socket);
             // - while the line that has read is not null (EOF was not sent), append the content to serverMessageTextView
             // by publishing the progress - with the publishProgress(...) method - to the UI thread
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                publishProgress(line);
+            }
             // - close the socket to the server
-
         } catch (Exception exception) {
             Log.e(Constants.TAG, "An exception has occurred: " + exception.getMessage());
             if (Constants.DEBUG) {
                 exception.printStackTrace();
+            }
+        }
+        finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+                Log.v(Constants.TAG, "Connection closed");
+            } catch (IOException ioException) {
+                Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
             }
         }
         return null;
@@ -39,12 +69,14 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
     protected void onPreExecute() {
         // TODO exercise 6b
         // - reset the content of the serverMessageTextView
+        serverMessageTextView.setText("");
     }
 
     @Override
     protected void onProgressUpdate(String... progress) {
         // TODO exercise 6b
         // - append the content to serverMessageTextView
+        serverMessageTextView.append(progress[0] + "\n");
     }
 
     @Override
